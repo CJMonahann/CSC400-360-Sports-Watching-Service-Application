cc30ffa2e8a399ea48c75bf223a4822069332f7d
Cornelius Monahan <corneliusmonahann@gmail.com>
2024-10-09 10:29:52 -0400

    added more imports and configurations for the database

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,18 @@ import os
 # force loading of environment variables
 load_dotenv('.flaskenv')
 
+DB_NAME = environ.get('SQLITE_DB')
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'CSC400'
+
+DB_CONFIG_STR = 'sqlite:///' + os.path.join(basedir, DB_NAME)
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONFIG_STR
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
+
+# Create database connection and associate it with the Flask application
+db = SQLAlchemy(app)
 
 # Add models
-from app import routes
+from app import routes, models
